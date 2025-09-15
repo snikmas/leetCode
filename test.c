6d4aba8e6cc2f82be30542e59505f00def3f40c0
MarySnik <mary.snikk@gmail.com>
2025-09-15 22:12:17 +0800

    add

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+  
+  printf("Menu\n");
+  printf("[1] - Choose A\n");
+  printf("[2] - Choose B\n");
+  printf("[3] - Choose C\n");
+  printf(">> ");
+
+  int n;
+  scanf("%i", &n);
+
+  return 0;
+}
