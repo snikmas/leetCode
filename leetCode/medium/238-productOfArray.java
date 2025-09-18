0eadea2545384e3e337b0461f49bc8893c13790b
MarySnik <mary.snikk@gmail.com>
2025-09-18 17:51:18 +0800

    nwe

---
+++ leetCode/medium/238-productOfArray.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        // 1. calculate preffix
+        res[0] = 1; 
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        // 2. calculate suffix
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+}
+ 238-productOfArray {
+  
+}
